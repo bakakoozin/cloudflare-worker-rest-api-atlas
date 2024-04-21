0307520de71fce041cddfcda9ce07f1e2a976afb
bakakoozin <cyrilpebre@gmail.com>
2024-04-21 12:58:44 +0200

    api_tests

--- api_tests/variables.sh
+++ api_tests/variables.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-worker_url="https://mongo-realm-worker.YOUR_SUBDOMAIN.workers.dev"
-api_key="YOUR_ATLAS_APP_AUTH_API_KEY"
+worker_url="https://mongo-realm-worker.cyrilpebre.workers.dev"
+api_key="ZDvF79yFwX4hO2DLOJmwi8JML3Uf5R2dmnXn65ABAPq8I5RKagfRz2ZSyZG4wP7s"
